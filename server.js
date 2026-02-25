ab5cfadcdf0766b82fc3a2e8e777f1a99c2a478f
mdridwan01 <mdridwan.github@gmail.com>
2026-02-25 15:56:16 +0600

    last origin change 3333

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.use((err, req, res, next) => {
 
 // Start server
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, '127.0.0.1', () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
