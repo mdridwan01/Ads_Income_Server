a1623ced8c306f0e2ca6a3fa5754da6967bd600e
mdridwan01 <mdridwan.github@gmail.com>
2026-02-26 10:36:09 +0600

    last uodae aws server run

--- server.js
+++ server.js
@@ -79,8 +79,10 @@ app.use((err, req, res, next) => {
 
 // Start server
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
+app.listen(PORT, '127.0.0.1', () => {
   console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
+
+//'127.0.0.1' aws c22
