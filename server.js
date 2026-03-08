d4cd08c588184cd3079c5181e6d798696e7c2e68
mdridwan01 <mdridwan.github@gmail.com>
2026-03-08 13:55:03 +0600

    last update ip address

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ app.use((err, req, res, next) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '127.0.0.1', () => {
   console.log(`Server running on port ${PORT}`);
 });
 
